c12b6f702efa1dbb28d285c063856af17781d846
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-03-15 13:54:15 +0100

    Add new traj

---
+++ attention_RMSE.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import os
+from utilities import load_object
+import math
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+for varname in os.listdir("train_attention"):
+    
+    print(varname)
+
+    final_train_RMSE = []
+    final_test_RMSE = []
+    final_val_RMSE = []
+    
+    all_mine = load_object("actual/actual_" + varname)
+    all_mine_flat = []
+    for filename in all_mine: 
+        for val in all_mine[filename]:
+            all_mine_flat.append(val)
+             
+    model_name = "GRU_Att"
+    ws_use = 1
+ 
+    final_val_data = pd.read_csv("train_attention/" + varname + "/predictions/val/" + model_name + "/" + varname + "_" + model_name + "_ws_" + str(ws_use) + "_val.csv", sep = ";", index_col = False)
+    final_val_data_predicted = [str(x).split(" ")[0].replace("a", ".") for x in final_val_data["predicted"]]
+    final_val_data_actual = [float(str(x).split(" ")[0].replace("a", ".")) for x in final_val_data["actual"]]
+
+    final_train_data = pd.read_csv("train_attention/" + varname + "/predictions/train/" + model_name + "/" + varname + "_" + model_name + "_ws_" + str(ws_use) + "_train.csv", sep = ";", index_col = False)
+    final_train_data_predicted = [str(x).split(" ")[0].replace("a", ".") for x in final_train_data["predicted"]]
+    final_train_data_actual = [float(str(x).split(" ")[0].replace("a", ".")) for x in final_train_data["actual"]]
+
+    final_test_data = pd.read_csv("train_attention/" + varname + "/predictions/test/" + model_name + "/" + varname + "_" + model_name + "_ws_" + str(ws_use) + "_test.csv", sep = ";", index_col = False)
+    final_test_data_predicted = [str(x).split(" ")[0].replace("a", ".") for x in final_test_data["predicted"]]
+    final_test_data_actual = [float(str(x).split(" ")[0].replace("a", ".")) for x in final_test_data["actual"]]
+ 
+    val_unk = 0
+    for i in range(len(final_val_data_predicted)):
+        if str(final_val_data_predicted[i]) == '<unk>':
+            val_unk += 1
+            if i > 0:
+                final_val_data_predicted[i] = final_val_data_predicted[i - 1]
+            else:
+                final_val_data_predicted[i] = 0
+        else:
+            final_val_data_predicted[i] = float(final_val_data_predicted[i])
+ 
+    final_val_RMSE.append(math.sqrt(mean_squared_error(final_val_data_actual, final_val_data_predicted)) / (max(all_mine_flat) - min(all_mine_flat)))
+
+    train_unk = 0
+    for i in range(len(final_train_data_predicted)):
+        if str(final_train_data_predicted[i]) == '<unk>':
+            train_unk += 1
+            if i > 0:
+                final_train_data_predicted[i] = final_train_data_predicted[i - 1]
+            else:
+                final_train_data_predicted[i] = 0
+        else:
+            final_train_data_predicted[i] = float(final_train_data_predicted[i])
+ 
+    final_train_RMSE.append(math.sqrt(mean_squared_error(final_train_data_actual, final_train_data_predicted)) / (max(all_mine_flat) - min(all_mine_flat)))
+
+    test_unk = 0
+    for i in range(len(final_test_data_predicted)):
+        if str(final_test_data_predicted[i]) == '<unk>':
+            test_unk += 1
+            if i > 0:
+                final_test_data_predicted[i] = final_test_data_predicted[i - 1]
+            else:
+                final_test_data_predicted[i] = 0
+        else:
+            final_test_data_predicted[i] = float(final_test_data_predicted[i])
+ 
+    final_test_RMSE.append(math.sqrt(mean_squared_error(final_test_data_actual, final_test_data_predicted)) / (max(all_mine_flat) - min(all_mine_flat)))
+
+    print(final_train_RMSE)
+    print(final_val_RMSE)
+    print(final_test_RMSE)
+
+    for val in final_test_RMSE:
+        print(np.round(val * 100, 2))
+
+    print(train_unk, len(final_train_data_predicted), np.round(train_unk / len(final_train_data_predicted) * 100, 4))
+    
+    print(val_unk, len(final_val_data_predicted), np.round(val_unk / len(final_val_data_predicted) * 100, 4))
+    
+    print(test_unk, len(final_test_data_predicted), np.round(test_unk / len(final_test_data_predicted) * 100, 4))
